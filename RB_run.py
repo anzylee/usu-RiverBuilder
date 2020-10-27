f9aff9e042350e4359ce1fadef6ca4a2626d59ae
anzylee <anzylee7@gmail.com>
2020-10-26 21:42:15 -0600

    update usu-RB

--- RB_run.py
+++ RB_run.py
@@ -1,12 +1,20 @@
+#############################################################################################
+# RB_run.py runs
+#   Written by Anzy Lee, Postdoctoral Scholar, Utah State University
+#   Date: 10/21/2020
+#############################################################################################
 import os
 from riverbuilder.core import river
 
-dir = "site316"
+#############################################################################################
+# Input: dir, case_name, fname, outfolder, log
+dir = "site316"                     # The name of directory 
 case_name = "site316_5x_cleanbed"
 os.chdir(dir)
 
 fname = case_name + "/" + case_name + ".txt"
 outfolder = case_name + "/" + case_name
 log = ""
+
 river.buildRiver(fname, outfolder, log)
 
