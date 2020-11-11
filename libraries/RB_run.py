0d88d87b38b511c7f1ad00df294063b74d6c1d8b
anzylee <anzylee7@gmail.com>
2020-11-11 00:01:08 -0600

    update

--- libraries/RB_run.py
+++ libraries/RB_run.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 # Input: dir, case_name
 dir = "../samples/site316"          # The name of directory you want to set
                                     # as a current working directory
-case_name = "site316_v4"   # Name for the case
+case_name = "site316_v5"   # Name for the case
 
 #############################################################################################
 # Optional inputs: fname, outfolder, log
